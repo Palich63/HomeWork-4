e8f0a152795ea9cbf90e7240a1f79284c1675081
Andrey Globa <Globa.Andrey@gmail.com>
2020-04-20 23:25:55 +0300

    #v2

--- src/main/java/ru/netology/domain/Comment.java
+++ src/main/java/ru/netology/domain/Comment.java
@@ -11,6 +11,35 @@ public class Comment {
     private int answerOnComment; // Ответить на комментарий
     private int SendComment; // Отправить сообщение
 
+    //Добавлено с vk
+    private int count;
+    private boolean canClose;
+    private boolean canOpen;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean isCanClose() {
+        return canClose;
+    }
+
+    public void setCanClose(boolean canClose) {
+        this.canClose = canClose;
+    }
+
+    public boolean isCanOpen() {
+        return canOpen;
+    }
+
+    public void setCanOpen(boolean canOpen) {
+        this.canOpen = canOpen;
+    }
+
     public int getSubscribe() {
         return subscribe;
     }
--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -6,11 +6,125 @@ public class Post {
     private int logo; // Владелец поста
     private int dropdownMenu; // Раскрывающееся меня
     private String text; // Текст поста
-    private Like like; // лайки
     private Share Share; // репост
     private Seen seen; // Количество просмотров
+    private Comment comment;
+    private Likes like; // лайки
 
-    private Comment comment ;
+
+    // Доработанно с vk
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private int replyOwnerId;
+    private int replyPostId;
+    private Reposts repost;
+    private String postType;
+    private PostSource postSource;
+    private Geo geo;
+    private int signerId;
+    private boolean isFavorite;
+    private int postPonedID;
+
+    public void setLike(Likes like) {
+        this.like = like;
+    }
+
+    public int getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+
+    public Reposts getRepost() {
+        return repost;
+    }
+
+    public void setRepost(Reposts repost) {
+        this.repost = repost;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public PostSource getPostSource() {
+        return postSource;
+    }
+
+    public void setPostSource(PostSource postSource) {
+        this.postSource = postSource;
+    }
+
+    public Geo getGeo() {
+        return geo;
+    }
+
+    public void setGeo(Geo geo) {
+        this.geo = geo;
+    }
+
+    public int getSignerId() {
+        return signerId;
+    }
+
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public int getPostPonedID() {
+        return postPonedID;
+    }
+
+    public void setPostPonedID(int postPonedID) {
+        this.postPonedID = postPonedID;
+    }
 
     public int getId() {
         return id;
