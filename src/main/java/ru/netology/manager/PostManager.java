21d488d4061502e17c2ad7ae1365981bdf81b3af
Andrey Globa <Globa.Andrey@gmail.com>
2020-04-20 23:52:44 +0300

    PostMamager

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -1,8 +1,19 @@
 package ru.netology.manager;
 
+import javafx.geometry.Pos;
 import ru.netology.domain.Post;
 
+import java.lang.reflect.Array;
+
 public class PostManager {
+
+//    private int ownerId;
+//    private String domain;
+//    private String query;
+//    private int[] ownersOnly;
+//    private int count;
+//    private int offset;
+//    private int postId;
     private Post[] posts;
 
     public Post[] getPosts() {
@@ -11,6 +22,16 @@ public class PostManager {
 
     public void setPosts(Post[] posts) {
         this.posts = posts;
+    }
+
+    public Post[] WallSearch(){
+
+        return null;
+    }
+
+    public void Post[] WallDelete(){
+
+
     }
 
 }
